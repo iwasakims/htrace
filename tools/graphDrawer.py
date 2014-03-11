70c8f74b491dbcee52b69b64d2cae0ed6a09bf92
iwasakims <iwasakims@example.com>
2014-03-11 09:47:24 -0700

    made runnable on python 2.6. assuming input spans are separated by newline.

--- tools/graphDrawer.py
+++ tools/graphDrawer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-  # Licensed to the Apache Software Foundation (ASF) under one or more
+# Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
 # The ASF licenses this file to You under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with
 # the License.  You may obtain a copy of the License at
-
-  #     http://www.apache.org/licenses/LICENSE-2.0
-
-  # Unless required by applicable law or agreed to in writing, software
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
@@ -36,32 +36,39 @@ def buildGraph(nid):
     gr.add_edge((nid, child))
     buildGraph(child)
 
-def loads_invalid_obj_list(s):
+def loads_spans(lines):
   decoder = JSONDecoder()
-  s_len = len(s)
   objs = []
-  end = 0
-  while end != s_len:
-    obj, end = decoder.raw_decode(s, idx=end)
+  for line in lines:
+    obj = decoder.decode(line)
     objs.append(obj)
   return objs
 
-nodes = loads_invalid_obj_list(sys.stdin.read().strip())
-spansBySpanId = {s["SpanID"]:s for s in nodes}
-spansByParent = defaultdict(set)
-
-for node in spansBySpanId.values():
-  spansByParent[node["ParentID"]].add(node["SpanID"])
-
-count = 0
-for x in spansByParent[ROOT_SPAN_ID]:
-  count += 1
-  gr = digraph()
-  gr.add_node(x, [("label", spansBySpanId[x]["Description"] + "(" + str(spansBySpanId[x]["Stop"] - spansBySpanId[x]["Start"]) +  ")")])
-  buildGraph(x)
-  dot = write(gr)
-  gvv = gv.readstring(dot)
-  gv.layout(gvv,'dot')
-  gv.render(gvv,'png','./graphs/' + str(datetime.now()) + str(spansBySpanId[x]["Description"])[:10] +   '.png')
-
-print("Created " + str(count)  + " images.")
+if __name__ == '__main__':
+  nodes = loads_spans(sys.stdin)
+  spansBySpanId = dict((s["SpanID"], s) for s in nodes)
+  spansByParent = defaultdict(set)
+  
+  for node in spansBySpanId.values():
+    spansByParent[node["ParentID"]].add(node["SpanID"])
+  
+  count = 0
+  for x in spansByParent[ROOT_SPAN_ID]:
+    count += 1
+    gr = digraph()
+    gr.add_node(x, [("label", spansBySpanId[x]["Description"]
+                     + "("
+                     + str(spansBySpanId[x]["Stop"] - spansBySpanId[x]["Start"])
+                     +  ")")])
+    
+    buildGraph(x)
+    dot = write(gr)
+    print dot
+    gvv = gv.readstring(dot)
+    gv.layout(gvv, 'dot')
+    gv.render(gvv, 'png',
+              datetime.now().isoformat() + '_'
+              + str(spansBySpanId[x]["Description"])[:10] + '.png')
+  
+  print("Created " + str(count)  + " images.")
+  
