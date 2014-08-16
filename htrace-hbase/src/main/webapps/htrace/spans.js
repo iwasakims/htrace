6417fda78bdd4ae70ce3059e644f771e2a7b8213
iwasakims <iwasakims@example.com>
2014-08-16 07:37:12 -0700

    wip.

--- htrace-hbase/src/main/webapps/htrace/spans.js
+++ htrace-hbase/src/main/webapps/htrace/spans.js
@@ -15,15 +15,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 var traceid = window.location.search.substring(1).split("=")[1];
 d3.json("/getspans/" + traceid, function(spans) {
+    var rootid = 477902;
     var maxwidth = 500;
     var left = 200;
     var tstart = d3.min(spans, function(s) {return s.start});
     var tstop = d3.max(spans, function(s) {return s.stop});
     var xscale = d3.scale.linear().domain([tstart, tstop]).range([0, maxwidth]);
 
+    /*
     gs = d3.select("svg")
       .selectAll("g")
       .data(spans)
@@ -57,4 +58,56 @@ d3.json("/getspans/" + traceid, function(spans) {
     gs.append("text")
       .text(function(s){return s.process_id})
       .style("alignment-baseline", "hanging");
+
+  */
+  var byparent = d3.nest()
+    .sortValues(function(a, b) {return a.start < b.start ? -1 : a.start > b.start ? 1 : 0;})
+    .key(function(e) {return e.parent_id}).map(spans, d3.map);
+  //console.log(byparent);
+  //var parents = d3.keys(byparent)
+  //console.log(parents);
+  addchildren(byparent.get(rootid), byparent);
+  console.log(byparent.get(rootid));
+
+  var packed = {span_id: "root", children: byparent.get(rootid)};
+  var treeChart = d3.layout.tree().size([500,500]);
+  var depthScale = d3.scale.category10([0,1,2]);
+  var linkGenerator = d3.svg.diagonal();
+  linkGenerator.projection(function (d) {return [d.y, d.x]})
+
+  d3.select("svg")
+    .append("g")
+    .attr("id", "treeG")
+    .selectAll("g")
+    .data(treeChart(packed))
+    .enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) {return "translate(" +d.y+","+d.x+")"});
+
+  d3.selectAll("g.node")
+    .append("circle")
+    .attr("r", 10)
+    .style("fill", function(d) {return depthScale(d.depth)})
+    .style("stroke", "white")
+    .style("stroke-width", "2px");
+
+  d3.selectAll("g.node")
+    .append("text")
+    .text(function(d) {return d.span_id || d.process_id || d.description});
+  
+  d3.select("#treeG").selectAll("path")
+    .data(treeChart.links(treeChart(packed)))
+    .enter().insert("path","g")
+    .attr("d", linkGenerator)
+    .style("fill", "none")
+    .style("stroke", "black")
+    .style("stroke-width", "2px");
   });
+
+function addchildren (children, byparent) {
+  children.forEach(function(e) {
+                       e.children = byparent.get(e.span_id);
+                       addchildren(e.children, byparent);
+                   });
+}
