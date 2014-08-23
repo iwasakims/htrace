c2b898f176c6877c7926adb346bf178f00e2009a
iwasakims <iwasakims@example.com>
2014-08-23 16:04:59 -0700

    added unit test for viewer.

--- htrace-hbase/src/test/java/org/htrace/viewer/TestHBaseSpanViewer.java
+++ htrace-hbase/src/test/java/org/htrace/viewer/TestHBaseSpanViewer.java
@@ -20,45 +20,76 @@ package org.htrace.viewer;
 import java.io.IOException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.hbase.HBaseTestingUtility;
-import org.apache.hadoop.hbase.client.HTableInterface;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Assert;
-import org.htrace.SpanReceiver;
-import org.htrace.TraceCreator;
-import org.htrace.impl.HBaseTestUtil;
-
+import org.htrace.protobuf.generated.SpanProtos.Span;
+import org.htrace.protobuf.generated.SpanProtos.TimelineAnnotation;
 
 public class TestHBaseSpanViewer {
   private static final Log LOG = LogFactory.getLog(TestHBaseSpanViewer.class);
-  private static final HBaseTestingUtility UTIL = new HBaseTestingUtility();
 
-  /*
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-    UTIL.startMiniCluster(1);
-  }
-
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {
-    UTIL.shutdownMiniCluster();
+  @Test
+  public void testProtoToJson() {
+    Span.Builder sbuilder = Span.newBuilder();
+    TimelineAnnotation.Builder tlbuilder = TimelineAnnotation.newBuilder();
+    sbuilder.clear().setTraceId(1)
+                    .setParentId(2)
+                    .setStart(3)
+                    .setStop(4)
+                    .setSpanId(5)
+                    .setProcessId("pid")
+                    .setDescription("description");
+    for (int i = 0; i < 3; i++) {
+      sbuilder.addTimeline(tlbuilder.clear()
+                           .setTime(i)
+                           .setMessage("message" + i)
+                           .build());
+    }
+    Span span = sbuilder.build();
+    try {
+      String json = HBaseSpanViewer.toJsonString(span);
+      String expected =
+          "{\"trace_id\":\"1\","
+          + "\"parent_id\":\"2\","
+          + "\"start\":\"3\","
+          + "\"stop\":\"4\","
+          + "\"span_id\":\"5\","
+          + "\"process_id\":\"pid\","
+          + "\"description\":\"description\","
+          + "\"timeline\":["
+          + "{\"time\":\"0\",\"message\":\"message0\"},"
+          + "{\"time\":\"1\",\"message\":\"message1\"},"
+          + "{\"time\":\"2\",\"message\":\"message2\"}]}";
+      Assert.assertEquals(json, expected);
+    } catch (IOException e) {
+      Assert.fail("failed to get json from span. " + e.getMessage());
+    }
   }
 
   @Test
-  public void testHBaseSpanViewer() {
-    HTableInterface htable = HBaseTestUtil.createTable(UTIL);
-    SpanReceiver receiver = HBaseTestUtil.startReceiver(UTIL);
-    TraceCreator tc = new TraceCreator(receiver);
-    tc.createThreadedTrace();
-    tc.createSimpleTrace();
-    tc.createSampleRpcTrace();
-    HBaseTestUtil.stopReceiver(receiver);
+  public void testProtoWithoutTimelineToJson() {
+    Span.Builder sbuilder = Span.newBuilder();
+    sbuilder.clear().setTraceId(1)
+                    .setParentId(2)
+                    .setStart(3)
+                    .setStop(4)
+                    .setSpanId(5)
+                    .setProcessId("pid")
+                    .setDescription("description");
+    Span span = sbuilder.build();
     try {
+      String json = HBaseSpanViewer.toJsonString(span);
+      String expected =
+          "{\"trace_id\":\"1\","
+          + "\"parent_id\":\"2\","
+          + "\"start\":\"3\","
+          + "\"stop\":\"4\","
+          + "\"span_id\":\"5\","
+          + "\"process_id\":\"pid\","
+          + "\"description\":\"description\"}";
+      Assert.assertEquals(json, expected);
     } catch (IOException e) {
-      Assert.fail("failed to get spans from HBase. " + e.getMessage());
+      Assert.fail("failed to get json from span. " + e.getMessage());
     }
   }
-  */
 }
